26acc7d87065ab97233d1f4c2d6d168cbab64901
Ahmet Utku Levent <ahmet.levent@etiya.com>
2022-11-28 20:47:39 +0300

    project init

---
+++ src/components/root/app/App.js
@@ -0,0 +1,13 @@
+import Navbar from "../../navbar/Navbar";
+
+function App() {
+  return (
+    <div >
+      <header>
+        <Navbar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
